cf2a3f76d27e4dc7b76ad20682ce123e640d3f8b
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-08-31 22:30:48 +0200

    blog posts

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -3,3 +3,4 @@
 LOCK_FILE="/tmp/blog.lock"
 HOME="/home/blog"
 flock -n $LOCK_FILE "$HOME/blog/scripts/deploy_if_changed.sh" >> "$HOME/logs/blog-deploy.log" 2>&1
+flock -n $LOCK_FILE "$HOME/blog/scripts/deploy_posts.sh" >> "$HOME/logs/blog-deploy.log" 2>&1
