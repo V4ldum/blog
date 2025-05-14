84e9d35daf2d3da5f4137cc25e507ae5b489a513
Valdum <56064897+V4ldum@users.noreply.github.com>
2025-05-14 16:00:12 +0200

    Update lock.sh

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-LOCK_FILE="/tmp/blog.lock"
+LOCK_FILE="/tmp/build.lock"
 HOME="/root/blog"
 flock -n $LOCK_FILE -c "bash $HOME/blog/scripts/deploy_if_changed.sh" >> "$HOME/logs/blog-deploy.log" 2>&1
